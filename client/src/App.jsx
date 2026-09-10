a0b41e2dba43f5a66bdd3a8761ac9df8207e2d73
Mithun Kumar <153515460+Mithun9661@users.noreply.github.com>
2026-09-10 11:11:15 +0530

    feat: enable AI animation from start to end of project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./AuthContext";
 import TopBar from "./components/TopBar";
+import AIGridBackground from "./components/AIGridBackground";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Predict from "./pages/Predict";
 import History from "./pages/History";
+import "./animation.css";
 
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <div className="app-shell">
+          <AIGridBackground />
           <TopBar />
           <Routes>
             <Route path="/" element={<Home />} />
